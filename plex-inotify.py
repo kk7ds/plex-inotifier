1ad8c5a21e72eb5d61bb8880b2336f6756bafa0d
Dan Smith <dsmith@danplanet.com>
2024-12-21 09:18:27 -0800

    Add webm to allowed_exts

--- plex-inotify.py
+++ plex-inotify.py
@@ -7,7 +7,8 @@
 allowed_exts = [
     'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff',
     'mp3', 'flac', 'aac', 'wma', 'ogg', 'ogv', 'wav', 'wma', 'aiff',
-    'mpg', 'mp4', 'avi', 'mkv', 'm4a', 'mov', 'wmv', 'm2v', 'm4v', 'vob'
+    'mpg', 'mp4', 'avi', 'mkv', 'm4a', 'mov', 'wmv', 'm2v', 'm4v', 'vob',
+    'webm',
 ]
 
 import argparse
